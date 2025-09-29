6b586f93aab3b886dd598343eee8908ae45e7d07
DalexAnthony <anthonydefernandez@gmail.com>
2025-09-28 22:33:17 -0600

    subiendo el juego

---
+++ index.js
@@ -0,0 +1,382 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+const boundaries = []
+const offset = {
+    x: -785,
+    y: -650
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1993)
+            boundaries.push(
+                new Boundary({
+                    position: {
+                        x: j * Boundary.width + offset.x,
+                        y: i * Boundary.height + offset.y
+                    }
+                }
+                ))
+    })
+})
+
+console.log(boundaries)
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = 'img/mapa.png'
+
+const foregroundImage = new Image()
+foregroundImage.src = 'img/foregroundObjects.png'
+
+const playerDownImage = new Image()
+playerDownImage.src = 'img/playerDown.png'
+
+const playerUpImage = new Image()
+playerUpImage.src = 'img/playerUp.png'
+
+const playerLeftImage = new Image()
+playerLeftImage.src = 'img/playerLeft.png'
+
+const playerRightImage = new Image()
+playerRightImage.src = 'img/playerRight.png'
+
+const npcImage = new Image()
+npcImage.src = 'img/player.png' // pon tu sprite quieto aquí
+
+const messageImage = new Image()
+messageImage.src = 'img/heartMessage.png' // el globo con corazón
+
+const playerFlowerImage = new Image()
+playerFlowerImage.src = 'img/playerFlower.png'
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - 192 / 4 / 2,
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerDownImage,
+    frames: {
+        max: 4
+    },
+    sprites: {
+        up: playerUpImage,
+        left: playerLeftImage,
+        right: playerRightImage,
+        down: playerDownImage
+    }
+})
+
+const npc = new Sprite({
+    position: {
+        x: offset.x + 600, // posición dentro del mapa (ajústalo a donde quieras que aparezca)
+        y: offset.y + 400
+    },
+    image: npcImage,
+    frames: { max: 4 }
+})
+
+const playerFlower = new Sprite({
+    position: {
+        x: npc.position.x,
+        y: npc.position.y
+    },
+    image: playerFlowerImage,
+    frames: { max: 4 }
+})
+
+// dibuja solo el primer frame
+playerFlower.draw = function () {
+    c.drawImage(
+        this.image,
+        0,
+        0,
+        this.image.width / 4,
+        this.image.height,
+        this.position.x,
+        this.position.y,
+        this.image.width / 4,
+        this.image.height
+    )
+}
+
+npc.draw = function () {
+    c.drawImage(
+        this.image,
+        0, // <-- usamos solo el primer frame (frame 0)
+        0,
+        this.image.width / 4, // divide entre 4 porque son 4 frames en fila
+        this.image.height,
+        this.position.x,
+        this.position.y,
+        this.image.width / 4,
+        this.image.height
+    )
+}
+
+let showMessage = false
+
+
+
+const background = new Sprite({
+    position: {
+        x: -785,
+        y: -650
+    },
+    image: image
+})
+
+const foreground = new Sprite({
+    position: {
+        x: -785,
+        y: -650
+    },
+    image: foregroundImage
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+
+
+const movables = [background, ...boundaries, npc, foreground]
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+    )
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+    })
+
+    if (!showMessage) {
+        // Caso normal: NPC en el mapa
+        npc.draw()
+    } else {
+        // Caso de interacción: aparece el jugador con flor + mensaje
+        playerFlower.position.x = npc.position.x
+        playerFlower.position.y = npc.position.y
+        playerFlower.draw()
+
+        c.drawImage(
+            messageImage,
+            npc.position.x - 20,
+            npc.position.y - 80,
+            100, 100
+        )
+    }
+
+    player.draw()
+
+    foreground.draw()
+
+    let moving = true
+    player.moving = false
+
+    if (keys.w.pressed && lastKey === 'w') {
+        player.moving = true
+        player.image = player.sprites.up
+
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y + 3
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+
+        if (moving)
+            movables.forEach(movable => {
+                movable.position.y += 3
+            })
+    } else if (keys.a.pressed && lastKey == 'a') {
+        player.moving = true
+        player.image = player.sprites.left
+
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x + 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+
+        if (moving)
+            movables.forEach(movable => {
+                movable.position.x += 3
+            })
+    } else if (keys.s.pressed && lastKey == 's') {
+        player.moving = true
+        player.image = player.sprites.down
+
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y - 3
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+
+        if (moving)
+            movables.forEach(movable => {
+                movable.position.y -= 3
+            })
+    } else if (keys.d.pressed && lastKey == 'd') {
+        player.moving = true
+        player.image = player.sprites.right
+
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x - 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+
+        if (moving)
+            movables.forEach(movable => {
+                movable.position.x -= 3
+            })
+    }
+
+}
+
+animate()
+
+let lastKey = ''
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+})
+
+canvas.addEventListener('click', (e) => {
+    const rect = canvas.getBoundingClientRect();
+
+    // factor de escala de CSS -> coordenadas internas (1024x576)
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+
+    const mouseX = (e.clientX - rect.left) * scaleX;
+    const mouseY = (e.clientY - rect.top) * scaleY;
+
+    if (
+        mouseX >= npc.position.x &&
+        mouseX <= npc.position.x + npc.width &&
+        mouseY >= npc.position.y &&
+        mouseY <= npc.position.y + npc.height
+    ) {
+        showMessage = !showMessage;
+    }
+});
